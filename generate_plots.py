839b9ecf7bd2ea11fe4407561a382fc2a7728a61
mbillouin2015 <mbillouin2015@my.fit.edu>
2020-04-01 17:32:01 -0400

    Example code for using plaidml

--- generate_plots.py
+++ generate_plots.py
@@ -8,7 +8,7 @@ from obspy.core.trace import Trace
 DATADIR = 'data/SMU_V5.pickle'
 SAVEDIR = 'plots/'
 
-sys.stdout = open('output.txt', 'w')
+# sys.stdout = open('output.txt', 'w')
 
 print('INFO: Reading Dataframe.')
 data = pd.read_pickle(DATADIR)
@@ -20,7 +20,6 @@ data.type.replace(to_replace='RMB', value='RMT', inplace=True)
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,2.5))
 
 for idx in trange(len(data)):
-    # idx = 10
     ax.clear()
     row = data.loc[idx]
     if row.type == 'RMT':
@@ -33,12 +32,7 @@ for idx in trange(len(data)):
     tr.data = row.ts
     tr.stats.starttime = row.datetime
     tr.stats.sampling_rate = row.hz
-    # tr.stats.station = row.station
-    # tr.stats.channel = row.chan
-    # tr.stats.network = row.net
     endtime = tr.stats.endtime
-
-    # tr.plot(color=color, outfile=SAVEDIR + row.type + '.png')
 
     t = np.arange(0.0, len(row.ts)/row.hz, 1/row.hz)
     ax.plot(t, row.ts, color=color)
@@ -52,3 +46,15 @@ for idx in trange(len(data)):
     # plt.show()
 plt.close()
 
+@jit
+def plotDF(df, save_dir):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,2.5))
+    for idx in trange(len(df)):
+        row = df.loc[idx]
+        ax.clear()
+        ax.plot(row.ts)
+        ax.set_xlim(xmin=0)
+        ax.set(ylabel='Amplitude', xlabel='Samples')
+        plt.savefig(save_dir +str(idx) + '.png')
+
+    plt.close(fig)
